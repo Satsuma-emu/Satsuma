d19b0c2f98e3941a08fd578c7ee47fbd9602453c
Catsoftware <leopold.schaffhauser@gmail.com>
2024-07-13 13:39:06 +0100

    Initial commit

---
+++ Satsuma/SatsumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SatsumaApp.swift
+//  Satsuma
+//
+//  Created by Léopold on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SatsumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
